d5a143b6967eddfde67bb015a4281a40dca483ca
Ahmed F Gadir <e17769@hotmail.com>
2024-07-28 13:45:02 -0400

    Refactoring

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,124 +1,94 @@
-import {
-  Image,
-  StyleSheet,
-  View,
-  Text,
-  TextInput,
-  Button
-} from "react-native";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import React, { useEffect, useState } from "react";
-//import { Icon } from 'react-native-elements';
+import React, { useEffect, useState } from 'react';
+import { Image, StyleSheet, View, Text, TextInput, Button } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
 
-export default  function HomeScreen() {
-  const [hours, setHours] = useState("");
-  const [minutes, setMinutes] = useState("");
-  const [resultHours, setResultHours] = useState("");
-  const [resultMinutes, setResultMinutes] = useState("");
-  const [resultStartTime, setStartTime] = useState("");
-  const [resultbloodDownPeriod, setbloodDownPeriod] = useState("");
-  const [resultbloodNormalPeriod, setbloodNormalPeriod] = useState("");
-  const [resultfatBurningPeriod, setfatBurningPeriod] = useState("");
-  const [resultketosisPeriod, setketosisPeriod] = useState("");
-  //Helper functions
-  function formatHoursToAMnPM(hours: number) {
-    const period = hours >= 12 ? "PM" : "AM";
-    // Convert hours from 24-hour to 12-hour format
+export default function HomeScreen() {
+  const [hours, setHours] = useState('');
+  const [minutes, setMinutes] = useState('');
+  const [resultHours, setResultHours] = useState('');
+  const [resultMinutes, setResultMinutes] = useState('');
+  const [resultStartTime, setStartTime] = useState('');
+  const [resultBloodDownPeriod, setBloodDownPeriod] = useState('');
+  const [resultBloodNormalPeriod, setBloodNormalPeriod] = useState('');
+  const [resultFatBurningPeriod, setFatBurningPeriod] = useState('');
+  const [resultKetosisPeriod, setKetosisPeriod] = useState('');
+
+  function formatHoursToAMnPM(hours: number, minutes: number) {
+    const period = hours >= 12 ? 'PM' : 'AM';
     const hours12 = hours % 12 || 12;
-    // Format the hours and minutes into a 12-hour time string
-    return `${hours12}:${minutes.toString().padStart(2, "0")} ${period}`;
+    return `${hours12}:${minutes.toString().padStart(2, '0')} ${period}`;
   }
- const retrieveDataFromStorage = async (key: string) => {
-  try {
-    const serializedValue = await AsyncStorage.getItem(key);
-    if (serializedValue !== null) {
-      return JSON.parse(serializedValue);
+
+  const retrieveDataFromStorage = async (key: string) => {
+    try {
+      const serializedValue = await AsyncStorage.getItem(key);
+      return serializedValue ? JSON.parse(serializedValue) : null;
+    } catch (error) {
+      console.error('Failed to retrieve data from AsyncStorage:', error);
+      return null;
     }
-  } catch (error) {
-    // Handle the error
-    console.error('Failed to retrieve data from AsyncStorage:', error);
-  }
-  return null; // or some default value
-};
-
- const storeData = async (key: string, value: any) => {
-  try {
-    const serializedValue = JSON.stringify(value);
-    await AsyncStorage.setItem(key, serializedValue);
-  } catch (error) {
-    // Handle the error
-    console.error('Failed to save data to AsyncStorage:', error);
-  }
- }
-  
-  const calculateFastingTime = (hours:number, minutes:number ) => {
-      let bloodDownPeriod = hours + 2;
-      let bloodNormalPeriod = hours + 8;
-      let fatBurningPeriod = hours + 10;
-      let ketosisPeriod = hours + 14;
-      let totalHours = hours + 16;
-      let totalMinutes = minutes;
-
-      // Adjust minutes if it exceeds 59
-      if (totalMinutes >= 60) {
-        totalHours += Math.floor(totalMinutes / 60);
-        totalMinutes = totalMinutes % 60;
-      }
-
-      // Adjust hours if it exceeds 23
-      if (totalHours >= 24) {
-        totalHours = totalHours % 24;
-      }
-      // Determine AM or PM suffix
-      let finalHour = formatHoursToAMnPM(totalHours);
-      // Update state with the result
-      setResultHours(finalHour);
-      setResultMinutes(totalMinutes.toString());
-      setStartTime(formatHoursToAMnPM(hours));
-      setbloodDownPeriod(formatHoursToAMnPM(bloodDownPeriod));
-      setbloodNormalPeriod(formatHoursToAMnPM(bloodNormalPeriod));
-      setfatBurningPeriod(formatHoursToAMnPM(fatBurningPeriod));
-      setketosisPeriod(formatHoursToAMnPM(ketosisPeriod));
-    }
-
-  console.log("Loading the app...");
-
-
-   // Use useEffect to load the value when the component mounts
-  useEffect(() => {
-    uploadFromLocalStorage();
-  }, []);
-  
-  const uploadFromLocalStorage = async () => {
-    console.log("Start getting shit from storae");
-    let h = await retrieveDataFromStorage("inputHours");
-    let m = await retrieveDataFromStorage("inputMinutes");
-    console.log("Time from storae", h, m)
-   setHours(h);
-    setMinutes(m);
-    let ih = parseInt(h) || 0;
-    let im = parseInt(m) || 0;
-    console.log("Time from storae", ih, im)
-    calculateFastingTime(ih,im); 
-  }
-  const handleAddTime = () => {
-    // Convert input to integers
-    let inputHours = parseInt(hours) || 0;
-    let inputMinutes = parseInt(minutes) || 0;
-    storeData("inputHours", inputHours);
-    storeData("inputMinutes",inputMinutes);
-    // Add  hours and calculate new time
-    calculateFastingTime(inputHours,inputMinutes);
   };
 
+  const storeData = async (key: string, value: any) => {
+    try {
+      await AsyncStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error('Failed to save data to AsyncStorage:', error);
+    }
+  };
+
+  const calculateFastingTime = (hours: number, minutes: number) => {
+    let bloodDownPeriod = hours + 2;
+    let bloodNormalPeriod = hours + 8;
+    let fatBurningPeriod = hours + 10;
+    let ketosisPeriod = hours + 14;
+    let totalHours = hours + 16;
+    let totalMinutes = minutes;
+
+    if (totalMinutes >= 60) {
+      totalHours += Math.floor(totalMinutes / 60);
+      totalMinutes %= 60;
+    }
+
+    if (totalHours >= 24) {
+      totalHours %= 24;
+    }
+
+    setResultHours(formatHoursToAMnPM(totalHours, totalMinutes));
+    setResultMinutes(totalMinutes.toString());
+    setStartTime(formatHoursToAMnPM(hours, minutes));
+    setBloodDownPeriod(formatHoursToAMnPM(bloodDownPeriod, minutes));
+    setBloodNormalPeriod(formatHoursToAMnPM(bloodNormalPeriod, minutes));
+    setFatBurningPeriod(formatHoursToAMnPM(fatBurningPeriod, minutes));
+    setKetosisPeriod(formatHoursToAMnPM(ketosisPeriod, minutes));
+  };
+
+  useEffect(() => {
+    const uploadFromLocalStorage = async () => {
+      const h = await retrieveDataFromStorage('inputHours');
+      const m = await retrieveDataFromStorage('inputMinutes');
+      setHours(h || '');
+      setMinutes(m || '');
+      calculateFastingTime(parseInt(h) || 0, parseInt(m) || 0);
+    };
+    uploadFromLocalStorage();
+  }, []);
+
+  const handleAddTime = () => {
+    const inputHours = parseInt(hours) || 0;
+    const inputMinutes = parseInt(minutes) || 0;
+    storeData('inputHours', inputHours);
+    storeData('inputMinutes', inputMinutes);
+    calculateFastingTime(inputHours, inputMinutes);
+  };
 
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: "#7c9177", dark: "#1D3D47" }}
+      headerBackgroundColor={{ light: '#7c9177', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require("@/assets/images/partial-react-logo.png")}
+          source={require('@/assets/images/partial-react-logo.png')}
           style={styles.reactLogo}
         />
       }
@@ -133,7 +103,7 @@ export default  function HomeScreen() {
             placeholder="00"
             keyboardType="numeric"
             value={hours}
-            onChangeText={(text) => setHours(text)}
+            onChangeText={setHours}
           />
           <Text style={[styles.colon, styles.shadowProp]}>:</Text>
           <TextInput
@@ -141,60 +111,28 @@ export default  function HomeScreen() {
             placeholder="00"
             keyboardType="numeric"
             value={minutes}
-            onChangeText={(text) => setMinutes(text)}
+            onChangeText={setMinutes}
           />
         </View>
         <Button title="Add 16 Hours" onPress={handleAddTime} />
         <View style={[styles.resultContainer, styles.shadowProp]}>
-          <Text style={[styles.resultDetailText]}>Start:Blood Sugar Rises</Text>
-          <Text
-            style={[
-              styles.result,
-              styles.shadowProp,
-              styles.bloodSugerUpPeriod,
-            ]}
-          >
-            {resultStartTime}
-          </Text>
+          <Text style={styles.resultDetailText}>Start: Blood Sugar Rises</Text>
+          <Text style={[styles.result, styles.bloodSugerUpPeriod]}>{resultStartTime}</Text>
 
-          <Text style={[styles.resultDetailText]}>
-            (2h):Blood Sugar goes down
-          </Text>
-          <Text
-            style={[styles.result, styles.shadowProp, styles.bloodDownPeriod]}
-          >
-            {resultbloodDownPeriod}
-          </Text>
+          <Text style={styles.resultDetailText}>(2h): Blood Sugar goes down</Text>
+          <Text style={[styles.result, styles.bloodDownPeriod]}>{resultBloodDownPeriod}</Text>
 
-          <Text style={[styles.resultDetailText]}>
-            (8h):Blood Sugar goes to normal
-          </Text>
-          <Text
-            style={[styles.result, styles.shadowProp, styles.bloodNormalPeriod]}
-          >
-            {resultbloodNormalPeriod}
-          </Text>
+          <Text style={styles.resultDetailText}>(8h): Blood Sugar goes to normal</Text>
+          <Text style={[styles.result, styles.bloodNormalPeriod]}>{resultBloodNormalPeriod}</Text>
 
-          <Text style={[styles.resultDetailText]}>
-            (10h):Fat Burn (yeahhh!)
-          </Text>
-          <Text
-            style={[styles.result, styles.shadowProp, styles.fatBurningPeriod]}
-          >
-            {resultfatBurningPeriod}
-          </Text>
+          <Text style={styles.resultDetailText}>(10h): Fat Burn (yeahhh!)</Text>
+          <Text style={[styles.result, styles.fatBurningPeriod]}>{resultFatBurningPeriod}</Text>
 
-          <Text style={[styles.resultDetailText]}>(14-16h):Ketosis</Text>
-          <Text
-            style={[styles.result, styles.shadowProp, styles.ketosisPeriod]}
-          >
-            {resultketosisPeriod}
-          </Text>
+          <Text style={styles.resultDetailText}>(14-16h): Ketosis</Text>
+          <Text style={[styles.result, styles.ketosisPeriod]}>{resultKetosisPeriod}</Text>
 
-          <Text style={[styles.resultDetailText]}>(16h):Autophagy</Text>
-          <Text style={[styles.result, styles.shadowProp, styles.autophagy]}>
-            {resultHours}
-          </Text>
+          <Text style={styles.resultDetailText}>(16h): Autophagy</Text>
+          <Text style={[styles.result, styles.autophagy]}>{resultHours}</Text>
         </View>
       </View>
     </ParallaxScrollView>
@@ -202,55 +140,55 @@ export default  function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
-  bloodSugerUpPeriod: { color: "#fc0a0a" },
-  bloodDownPeriod: { color: "#ada824" },
-  bloodNormalPeriod: { color: "#f8fc0a" },
-  fatBurningPeriod: { color: "#fa6502" },
-  ketosisPeriod: { color: "#62ad24" },
-  autophagy: { color: "#2499ad" },
+  bloodSugerUpPeriod: { color: '#fc0a0a' },
+  bloodDownPeriod: { color: '#ada824' },
+  bloodNormalPeriod: { color: '#f8fc0a' },
+  fatBurningPeriod: { color: '#fa6502' },
+  ketosisPeriod: { color: '#62ad24' },
+  autophagy: { color: '#2499ad' },
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     paddingHorizontal: 20,
-    backgroundColor: "#000000",
+    backgroundColor: '#000000',
   },
   header: {
     fontSize: 32,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginBottom: 20,
-    textAlign: "center",
-    color: "#a1f9ff",
+    textAlign: 'center',
+    color: '#a1f9ff',
     opacity: 0.88,
   },
   inputContainer: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     marginBottom: 20,
   },
   inputHours: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     width: 80,
     height: 40,
     opacity: 0.88,
-    borderColor: "#748071",
+    borderColor: '#748071',
     borderWidth: 1,
     paddingHorizontal: 10,
-    textAlign: "center",
+    textAlign: 'center',
     fontSize: 18,
-    color: "#a1f9ff",
+    color: '#a1f9ff',
   },
   inputMM: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     width: 80,
     height: 40,
     opacity: 0.88,
-    borderColor: "#748071",
+    borderColor: '#748071',
     borderWidth: 1,
     paddingHorizontal: 10,
-    textAlign: "center",
+    textAlign: 'center',
     fontSize: 18,
-    color: "#a1f9ff",
+    color: '#a1f9ff',
   },
   colon: {
     fontSize: 24,
@@ -258,37 +196,22 @@ const styles = StyleSheet.create({
   },
   resultContainer: {
     marginTop: 20,
-    alignItems: "center",
-    color: "#a1f9ff",
-  },
-  resultText: {
-    fontSize: 18,
-    color: "#a1f9ff",
-    fontWeight: "bold",
-    opacity: 0.88,
+    alignItems: 'center',
+    color: '#a1f9ff',
   },
   resultDetailText: {
     fontSize: 18,
-    color: "#a1f9ff",
-    fontWeight: "bold",
+    color: '#a1f9ff',
+    fontWeight: 'bold',
   },
   result: {
     fontSize: 24,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginTop: 10,
     opacity: 0.88,
   },
   reactLogo: {},
-  Shit: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-  },
   shadowProp: {
-/*     textShadowColor: "#6e7d6a",
-    shadowOpacity: 0.1,
-    textShadowRadius: 1, */
+    // Add shadow properties if necessary
   },
 });
